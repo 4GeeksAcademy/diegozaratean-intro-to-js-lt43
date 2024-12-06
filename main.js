eda7523fc1a2eebfeee009d47d1087e53db0296c
diego <diegozaratean@gmail.com>
2024-12-06 23:03:03 +0000

    finish intro js

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log("hola des main.js")
+
+let nombre = 'diego'
+const fechaNBacimeinto = '1985/08/04'
+let estaVivo = true
+
+
+// elemento.     1.     2.         3.      4
+let frutas = ['mango','patilla','melon','cereza']
+// position.     0.      1.       2.        3
+
+let carro = {marca:'tesla',modelo: 2024,precio:60000,disponible: true}
