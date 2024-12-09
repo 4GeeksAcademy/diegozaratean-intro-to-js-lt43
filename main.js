fed4350424d4f0dfad8422c1d01b929c0a66264c
diego <diegozaratean@gmail.com>
2024-12-09 22:32:03 +0000

    finish excuse generator

--- main.js
+++ main.js
@@ -1,12 +1,65 @@
 console.log("hola des main.js")
 
 let nombre = 'diego'
-const fechaNBacimeinto = '1985/08/04'
-let estaVivo = true
+
+// console.log('QUEMADO')
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+
+console.log('con el FOR')
+// for(inicialización;condicion;actualizacion
+// cuantas veces tengo que ejecutarlo 5 verces
+// de cada cuanto o cauntos son los saltos. 1 en 1
+// cual es el valor inicial. 1
+
+//. i = 1 ==> 1
+//. i = 2 ==> 2
+//. i = 3 ==> 4
+//. i = 4 ==> 4
+//. i = 5 ==> 5
+//. i = 6 
+// for(i=1; i  <= 50  ; i++){
+//     console.log(i)
+// } 
 
 
-// elemento.     1.     2.         3.      4
-let frutas = ['mango','patilla','melon','cereza']
-// position.     0.      1.       2.        3
+// for(i=20; i >= 15;i--){
+//     console.log(i)
+// }
+// console.log('con el FOR. de 30 a 50')
+// for(i = 30 ; i <= 50 ; i = i + 5){
+//     console.log(i)
+// }
 
-let carro = {marca:'tesla',modelo: 2024,precio:60000,disponible: true}
+// let nombres = ['diego','johana','santiago']
+// console.log(nombres)
+// nombres.push('luis')
+// console.log(nombres)
+
+// const alumnos = ['adler','alejandro','christian']
+// console.log(alumnos)
+// alumnos.push('eduardo')
+// console.log(alumnos)
+
+const frutas = ['pera','platano','manzana']
+console.log(frutas)
+console.log(frutas.length)
+
+
+for(i= 0 ; i < frutas.length; i++){
+    console.log(frutas[i] + 's')
+}
+
+
+// num = num + 5 ======== num+=5
+
+
+
+
+
+
+
+
