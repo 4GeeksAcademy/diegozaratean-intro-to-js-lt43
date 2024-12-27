ae1d3da7e552dada5894e5193e70b20b676904b3
diego <diegozaratean@gmail.com>
2024-12-27 22:42:33 +0000

    finish dom class

--- main.js
+++ main.js
@@ -1,40 +1,48 @@
 console.log("hola des main.js")
 
-// realicen un programa que tome el sueldo de un usuairo y le calcule el impuesto 
-// si es menor a 1000 se debe indicar "Debes pagar el 5%" si es mayor a 1000 se debe indicar
-// "Debes pagar el 10%"
+const persona = {
+  nombre: 'Adler',
+  apellido: "Sejousnat",
+  edad: 33,
+  skills: ['js','react','python'],
+  programar: function(){
+    console.log('estoy programando')
+  }
+}
+
+const arrayEjemplo = [ 'Adler',"Sejousnat"]
+
+const mascota = {
+  nombre: 'gordo',
+  raza: 'chitzu',
+  color: ['blanco','cafe'],
+  ladrar: function(){
+    console.log('wow')
+  }
+}
+
+
+
 
 // PSEUDOCODIGO
-// definir variable sueldo
-let sueldo = 400
-// // validar si sueldo es mayor o no a 1000
-// let mensaje = ''
-// if( sueldo < 1000){
-//     // hago algo:  debo imprimir "Debes pagar el 5%" s
-//    mensaje = "Debes pagar el 5%"
-// }else{
-//     // hago otra cosa:  debo imprimir "Debes pagar el 10%" s
-//     mensaje = "Debes pagar el 10%"
-// }
-// mostrar el resultado (consola)
-// console.log(mensaje)
+// DONE: crear el boton en index.html
+// DONE: cuando le de click al boton se ejecute una acción
+// DONE: crear funcion que cambie titulo
+function cambiarTitulo(){
+  console.log('cambiarTitulo')
+  // DONE:  cambiar el texto del titulo
+  document.querySelector('#title').innerHTML = 'nuevo titulo' 
+  // DONE: cambiar el color del titulo
+  document.getElementById('title').style.backgroundColor = 'blue' 
+}
+// cambiarTitulo()
 
-// Ternary operator
-// if(condicion){
-//     que hago si verdadero
-// }else{
-//     que hago si falso
-// }
-//(condicion) ?que hago si verdadero : que hago si falso
-let mensaje =  ( sueldo < 1000) ? "Debes pagar el 5%" : "Debes pagar el 10%"
-console.log(mensaje)
-let content = document.getElementById('content')
 
-content.insertAdjacentHTML(
-    'beforeEnd',
-    `<h1>${mensaje}</h1>`,
-  );
-// CASOS DE USO:
-// 400 => "Debes pagar el 5%"
-// 1200 => "Debes pagar el 10%"
-// 1000 => "Debes pagar el 10%"
+
+
+
+
+
+
+
+
