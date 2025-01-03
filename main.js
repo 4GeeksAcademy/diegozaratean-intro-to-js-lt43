aa93d6630e593f509e4ed0db58ec200bd2cc55a8
diego <diegozaratean@gmail.com>
2025-01-03 23:01:34 +0000

    finish dom class

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const persona = {
   apellido: "Sejousnat",
   edad: 33,
   skills: ['js','react','python'],
+  "lugar de nacimiento": 'haiti',
   programar: function(){
     console.log('estoy programando')
   }
@@ -29,18 +30,25 @@ const mascota = {
 // DONE: cuando le de click al boton se ejecute una acción
 // DONE: crear funcion que cambie titulo
 function cambiarTitulo(){
-  console.log('cambiarTitulo')
-  // DONE:  cambiar el texto del titulo
-  document.querySelector('#title').innerHTML = 'nuevo titulo' 
-  // DONE: cambiar el color del titulo
-  document.getElementById('title').style.backgroundColor = 'blue' 
+  let colores = ['red','blue','yellow','violet','green','aqua']
+  let nombres = ['Adler','Matias','Alejandro','Franco','Martin','Oscar']
+  let randomColor = colores[ Math.floor(Math.random() * colores.length) ]
+  document.getElementById('title').style.backgroundColor = randomColor
+  let randomName = nombres[ Math.floor(Math.random() * nombres.length) ] 
+  document.querySelector('#title').innerHTML = 'hola ' +randomName
 }
 // cambiarTitulo()
 
 
-
-
-
+// PSEUDOCODIGO
+// realizar un programa que simule dos(2) dados y tenga un boton para lanzar los dados
+function lanzarDados(){
+  let dice1 = Math.floor( Math.random() * 6) + 1
+  let dice2 = Math.ceil( Math.random() * 6) 
+  document.getElementById('dado1').innerHTML = dice1
+  document.getElementById('dado2').innerHTML = dice2
+}
+// agregar estilo
 
 
 
